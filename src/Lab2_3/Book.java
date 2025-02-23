50c55def9ee72b65e023bdaf3b39072612ccd590
nminnnnn <nguyennhatminh140@gmail.com>
2025-02-23 09:25:31 +0700

    push Lab2_3

---
+++ src/Lab2_3/Book.java
@@ -0,0 +1,76 @@
+package Lab2_3;
+
+public class Book {
+	private String isbn;
+	private String name;
+	private Author author;
+	private double price;
+	private int qty = 0;
+	
+	public Book(String isbn, String name, Author author, double price) {
+		this.isbn = isbn;
+		this.name = name;
+		this.author = author;
+		this.price = price;
+	}
+
+	public Book(String isbn, String name, Author author, double price, int qty) {
+		this.isbn = isbn;
+		this.name = name;
+		this.author = author;
+		this.price = price;
+		this.qty = qty;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+//	public void setIsbn(String isbn) {
+//		this.isbn = isbn;
+//	}
+
+	public String getName() {
+		return name;
+	}
+
+//	public void setName(String name) {
+//		this.name = name;
+//	}
+
+	public Author getAuthor() {
+		return author;
+	}
+
+//	public void setAuthor(Author author) {
+//		this.author = author;
+//	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getQty() {
+		return qty;
+	}
+
+	public void setQty(int qty) {
+		this.qty = qty;
+	}
+	public String getAuthorName() {
+		return author.getName();
+	}
+
+	@Override
+	public String toString() {
+		return "Book [isbn=" + isbn + ", name=" + name + ", " + author.toString() + ", price=" + price + ", qty=" + qty
+				+ "]";
+	}
+	
+	
+	
+}
